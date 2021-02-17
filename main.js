d8af265459acf08a5ddcfac1cbea4b55ba479ae5
Paulo Queirós <paulogomes.queiros@gmail.com>
2021-02-17 03:15:15 -0300

    implementado codigo escrito borda top right

--- main.js
+++ main.js
@@ -4,16 +4,28 @@ function AlteraBorda1(){
     let res = valueString.match(/^[0-9]*/gi);
     console.log(res+"px");
     document.querySelector('.container').style.borderTopLeftRadius = res+"px";
+    let reg = /[0-9]+px/gi;
     document.querySelector('textarea').innerHTML = "- border-top-left-radius: " + res+"px;\n";
+    document.querySelector('textarea').innerHTML.replace(reg, res+"px");
 
 };
 function AlteraBorda2(){
     let value = document.querySelector('.input2').value;
     valueString = value.toString();
     let res = valueString.match(/^[0-9]*/gi);
-    console.log(res+"px");
-    document.querySelector('.container').style.borderTopRightRadius = res+"px"; 
-    document.querySelector('textarea').innerHTML = " - border-top-right-radius: " + res+"px;"; 
+    
+    document.querySelector('.container').style.borderTopRightRadius = res+"px";
+
+    let codeBorda2 = /-\sborder-top-right-radius:\s[0-9]+px;/gi
+    
+    let texto = document.querySelector('textarea').innerHTML;
+
+    if(codeBorda2.test(texto)){
+        document.querySelector('textarea').innerHTML = texto.replace(codeBorda2, "- border-top-right-radius: " + res+"px;\n");
+    }else{
+        document.querySelector('textarea').innerHTML = texto + "- border-top-right-radius: " + res+"px;\n";
+    }
+    
 };
 function AlteraBorda3(){
     let value = document.querySelector('.input3').value;
@@ -21,7 +33,7 @@ function AlteraBorda3(){
     let res = valueString.match(/^[0-9]*/gi);
     console.log(res+"px");
     document.querySelector('.container').style.borderBottomLeftRadius = res+"px"; 
-    document.querySelector('textarea').innerHTML = " - border-bottom-left-radius: " + res+"px;"; 
+    document.querySelector('textarea').innerHTML = "- border-bottom-left-radius: " + res+"px;"; 
 };
 function AlteraBorda4(){
     let value = document.querySelector('.input4').value;
@@ -29,6 +41,6 @@ function AlteraBorda4(){
     let res = valueString.match(/^[0-9]*/gi);
     console.log(res+"px");
     document.querySelector('.container').style.borderBottomRightRadius = res+"px"; 
-    document.querySelector('textarea').innerHTML = " - border-bottom-right-radius: " + res+"px;"; 
+    document.querySelector('textarea').innerHTML = "- border-bottom-right-radius: " + res+"px;"; 
 };
 
